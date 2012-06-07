33ece7bc91a32ebd6eb243e0cc3fe498dc27f030
Jos Bolssens (@ imac OWS) <marvelade@gmail.com>
2012-06-07 13:35:00 +0200

    first commit

---
+++ Controlar.ino
@@ -0,0 +1,32 @@
+/*
+  ReadAnalogVoltage
+  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+ 
+ This example code is in the public domain.
+ */
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  int sensorMaxValue = 770;
+  //sensorValue = 100*sensorValue/sensorMaxValue;
+  
+  int limmin = analogRead(A1);
+  int limmax = analogRead(A2);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  //float voltage1 = sensorValue + (pow(10, 2*(sensorValue-sensorMaxValue)));
+  float voltage1 = sensorValue + 2;
+  // print out the value you read:
+  Serial.println(voltage1);
+  Serial.println(sensorValue);
+  Serial.println("");
+  delay(250);
+}
