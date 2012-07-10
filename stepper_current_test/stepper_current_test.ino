e1c372b07dca5a3779aea3abcc2cebb735cb7e97
Jos Bolssens (@ imac OWS) <marvelade@gmail.com>
2012-07-10 13:25:53 +0200

    added motor current measurement testing program

---
+++ stepper_current_test/stepper_current_test.ino
@@ -0,0 +1,62 @@
+// Include the Stepper Library
+#include <Stepper.h>
+
+
+// Map our pins to constants to make things easier to keep track of
+const int pwmA = 3;
+const int pwmB = 11;
+const int brakeA = 9;
+const int brakeB = 8;
+const int dirA = 12;
+const int dirB = 13;
+
+// The amount of steps for a full revolution of your motor.
+// 360 / stepAngle
+const int STEPS = 200;
+
+// Initialize the Stepper class
+Stepper myStepper(STEPS, dirA, dirB);
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // Set the RPM of the motor
+  myStepper.setSpeed(300);
+
+  // Turn on pulse width modulation
+  pinMode(pwmA, OUTPUT);
+  digitalWrite(pwmA, HIGH);
+  pinMode(pwmB, OUTPUT);
+  digitalWrite(pwmB, HIGH);
+
+  // Turn off the brakes
+  pinMode(brakeA, OUTPUT);
+  digitalWrite(brakeA, LOW);
+  pinMode(brakeB, OUTPUT);
+  digitalWrite(brakeB, LOW);
+
+  // Log some shit
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  
+   float currentA = analogRead(A0);
+   float currentB = analogRead(A1);
+   float totalCurrent = currentA+currentB;
+   
+   Serial.print("current: ");
+   Serial.println(totalCurrent);
+   if(totalCurrent > 200.0)
+   {
+     Serial.println("Current too high. Stalling.");
+   }
+   else
+   {
+       
+     Serial.println("Current OK. Stepping.");
+     myStepper.step(11);
+   }
+  
+  delay(100);
+}
